74f5f23ef450100a1fa3c420ce92a108bfbd441c
mimo-lgtm <shoji.yano@gmail.com>
2026-06-19 23:17:43 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // ======================= 設定 =======================
-const GAS_URL = "https://script.google.com/macros/s/AKfycbx5CeFPIECTBsi1tRF-tIeQy9YL7xP8dDtvpyF7nTOjGRdTQHPpOHT4iW4h0rHqaPvP_w/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbx5CeFPIECTBsi1tRF-tIeQy9YL7xP8dDtvpyF7nTOjGRdTQHPpOHT4iW4h0rHqaPvP_w/ehttps://script.google.com/macros/s/AKfycbx5CeFPIECTBsi1tRF-tIeQy9YL7xP8dDtvpyF7nTOjGRdTQHPpOHT4iW4h0rHqaPvP_w/exec";
 
 // 状態管理
 let currentCategory = "① 芦屋市の価値向上";
