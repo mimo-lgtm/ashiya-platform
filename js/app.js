3170977fa211fc3ae161f29408f69691fe3c7093
mimo-lgtm <shoji.yano@gmail.com>
2026-06-21 16:14:37 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // ======================= 設定 =======================
-const GAS_URL = "https://script.google.com/macros/s/AKfycbyLuvNvF3zQd1sGgcOaSucshBroWQ-pssSKlrdlAOZZdDGedLY42Mr6Hy23gdA30wW2Ag/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbx5NfZU7RuQcTGr0qCHlr5i7YOT_iq8v-NyY1tIrTUGdneGdzKUfadSTv7qdRBFBib6Ng/exec";
 
 // ======================= 状態管理 =======================
 let currentCategory = "① 芦屋市の価値向上（ブランド・移住促進）";
