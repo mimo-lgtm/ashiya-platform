94a8e494bac7ca3c7b45a985017c5a9129b96326
mimo-lgtm <shoji.yano@gmail.com>
2026-06-24 21:13:46 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // ======================= 設定 =======================
-const GAS_URL = "https://script.google.com/macros/s/AKfycbymx19ORq63wrfnsScaM_Ma380eSNrTjRc1maCCtwDQdeOSKXESuh8iRcAKFm1D78LBpQ/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxcTy2d3087f0xTxDMe0eko3lfTlcINk-lkp53eQNw4vNDS31NW95MXCae0mOnHRDUL8A/exec";
 
 const OTHER_LABEL = "その他";
 
